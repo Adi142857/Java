5eb948fe500cb9fd4fd6c7016be45859c1aa9d5d
Aditya <65461420+Adi142857@users.noreply.github.com>
2020-09-30 23:58:57 +0530

    Updated  with variables
    Optimised

--- Boolean.java
+++ Boolean.java
@@ -1,12 +1,9 @@
-public class Boolean{
-	
+public class Boolean
+{
 	public static void main(String args[])
 	{
-		int x=6;
-		int y=-6;
-		System.out.println(""+x);
-		System.out.println(""+y);
-		System.out.println(""+(x>>2));
-		System.out.println(""+(x<<1));
+		int x=5,y=-6;
+		System.out.println(""+x+""+y);
+		System.out.println(""+(x>>2)+""+(x<<1));
 	}
 }
